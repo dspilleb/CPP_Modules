14d6d589668ffe3ee92837d6148755a695689f79
Dan <spillebeendanilo@gmail.com>
2023-11-22 19:20:44 +0100

    base files ex04

---
+++ CPP01/ex04/Replace.hpp
@@ -0,0 +1,10 @@
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+# include <iostream>
+
+# define RED "\033[1;31m"
+# define NONE "\033[0m"
+
+
+#endif
---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 19:13:59 by dspilleb          #+#    #+#             */
+/*   Updated: 2023/11/22 19:19:56 by dspilleb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//mauvais filename
+#include "Replace.hpp"
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	if (ac != 4)
+		std::cout << RED << "Error : Wrong number of arguments" << std::endl;
+}
