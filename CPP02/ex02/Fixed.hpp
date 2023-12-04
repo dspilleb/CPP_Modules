6fb86ecb2b88418fb14de5c62ff5867b2915ead6
Dan <spillebeendanilo@gmail.com>
2023-12-04 11:33:49 +0100

    Ex02 + rename variable ex01

--- CPP02/ex02/Fixed.hpp
+++ CPP02/ex02/Fixed.hpp
@@ -2,6 +2,7 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
@@ -11,10 +12,16 @@ class Fixed
 	public:
 		Fixed ( void );
 		Fixed ( Fixed const &ref);
+		Fixed (const int init_val);
+		Fixed (const float init_val);
 		Fixed & operator=(Fixed const & rhs);
 		~Fixed ( void );
 		int getRawBits( void ) const;
+		int get_point_bit_position( void ) const;
 		void setRawBits( int const raw );
-};
+		float toFloat( void ) const;
+		int toInt( void ) const;
+	};
 
+std::ostream& operator<<(std::ostream &os, Fixed const &ref);
 #endif
