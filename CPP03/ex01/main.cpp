33d07aff2d664e66bbc0aa6a2a94b6c3ed1b0d2a
Dan <spillebeendanilo@gmail.com>
2023-12-09 14:11:01 +0100

    better tests

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,29 +6,61 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:21:39 by dspilleb          #+#    #+#             */
-/*   Updated: 2023/12/08 11:26:00 by dspilleb         ###   ########.fr       */
+/*   Updated: 2023/12/09 14:08:33 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
+using std::cout;
+using std::endl;
+
+
+void test_death( void )
+{
+	cout << "----TEST DEATH----" << std::endl;
+	{
+		ScavTrap test_death ("test_death");
+
+		test_death.takeDamage(10);
+		test_death.attack("42");
+		test_death.takeDamage(90);
+		test_death.attack("42");
+		test_death.beRepaired(19);
+		test_death.takeDamage(19);
+	}
+	cout << "------------------" << std::endl;
+}
+
+void test_energy( void )
+{
+	cout << "----TEST ENERGY----" << std::endl;
+	{
+		ScavTrap test_energy ("test_energy");
+
+		for (int i = 0; i < 51; i++)
+			test_energy.attack("test_energy");
+		test_energy.beRepaired(1);
+	}
+	cout << "------------------" << std::endl;
+}
+
+void test_special_ability( void )
+{
+	cout << "----TEST ABILITY----" << std::endl;
+	{
+		ScavTrap test_ability ("test_ability");
+
+		test_ability.guardGate();
+	}
+	cout << "------------------" << std::endl;
+}
+
 int	main ( void )
 {
-	ScavTrap test ("ScavitoTrapito");
-	ClapTrap test2 ("ClapTrapito");
-
-	test.attack("ClapTrapito");
-	test2.takeDamage(20);
-	test2.attack("ScavitoTrapito");
-	test2.takeDamage(2);
-	test.beRepaired(2);
-	test2.beRepaired(2);
-	test.guardGate();
-	test.takeDamage(1);
-	ScavTrap test3;
-	test3 = test;
-	test3.beRepaired(1);
-	test.beRepaired(1);
+	test_death();
+	test_energy();
+	test_special_ability();
 	return (0);
 }
--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:21:39 by dspilleb          #+#    #+#             */
-/*   Updated: 2023/12/08 12:05:58 by dspilleb         ###   ########.fr       */
+/*   Updated: 2023/12/09 14:10:10 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,54 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
+using std::cout;
+using std::endl;
+
+
+void test_death( void )
+{
+	cout << "----TEST DEATH----" << std::endl;
+	{
+		FragTrap test_death ("test_death");
+
+		test_death.takeDamage(10);
+		test_death.attack("42");
+		test_death.takeDamage(90);
+		test_death.attack("42");
+		test_death.beRepaired(19);
+		test_death.takeDamage(19);
+	}
+	cout << "------------------" << std::endl;
+}
+
+void test_energy( void )
+{
+	cout << "----TEST ENERGY----" << std::endl;
+	{
+		FragTrap test_energy ("test_energy");
+
+		for (int i = 0; i < 101; i++)
+			test_energy.attack("test_energy");
+		test_energy.beRepaired(1);
+	}
+	cout << "------------------" << std::endl;
+}
+
+void test_special_ability( void )
+{
+	cout << "----TEST ABILITY----" << std::endl;
+	{
+		FragTrap test_ability ("test_ability");
+
+		test_ability.highFivesGuys();
+	}
+	cout << "------------------" << std::endl;
+}
+
 int	main ( void )
 {
-	FragTrap test = FragTrap("Fragito");
-	test.highFivesGuys();
-	test.attack("Nobody");
-	test.beRepaired(0);
-	test.takeDamage(0);
+	test_death();
+	test_energy();
+	test_special_ability();
 	return (0);
 }
