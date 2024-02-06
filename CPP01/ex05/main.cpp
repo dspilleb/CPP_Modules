d216bf7d32f9393c86c2748ade98b0bd9f622796
Dan <spillebeendanilo@gmail.com>
2024-02-06 20:59:21 +0100

    better naming

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:48:52 by dspilleb          #+#    #+#             */
-/*   Updated: 2023/12/10 20:14:18 by dspilleb         ###   ########.fr       */
+/*   Updated: 2024/02/06 20:58:57 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 		std::cout << "Veuillez introduire un niveau de plainte de HARL :\n'DEBUG' | 'INFO' | 'WARNING' | 'ERROR'" << std::endl;
 		return (1);
 	}
-	Harl test;
-	test.complain(av[1]);
+	Harl my_harl;
+	my_harl.complain(av[1]);
 	return (0);
 }
