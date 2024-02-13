bf6fa2d16df491bd6d31dd0acb4f14f1981353fe
Dan <spillebeendanilo@gmail.com>
2024-02-13 11:42:30 +0100

    new test

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:06:27 by dspilleb          #+#    #+#             */
-/*   Updated: 2023/12/02 15:12:53 by dspilleb         ###   ########.fr       */
+/*   Updated: 2024/02/13 11:41:41 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,24 @@
 
 int main ( void )
 {
-	Fixed a;
-	Fixed b( a );
-	Fixed c;
+	{
+		Fixed a;
+		Fixed b( a );
+		Fixed c;
 
-	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+		c = b;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
+	{
+		std::cout << "\n----TEST 2 :" << std::endl;
+		Fixed a;
+
+		std::cout << a.getRawBits() << std::endl;
+		a.setRawBits(42);
+		std::cout << a.getRawBits() << std::endl; // should be 42
+	}
 
 	return 0;
 }
