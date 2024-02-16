ec59ab0e607fe92238d30f275c031a9f2177cfe8
dspilleb <spillebeendanilo@gmail.com>
2024-02-16 11:24:16 +0100

    Functions not static

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:19:40 by dspilleb          #+#    #+#             */
-/*   Updated: 2024/02/14 12:10:36 by dspilleb         ###   ########.fr       */
+/*   Updated: 2024/02/16 11:23:22 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(void)
 	Zombie Zombie1 = Zombie("Zombie1");
 	Zombie1.announce();
 
-	Zombie::randomChump("randomChump");
+	randomChump("randomChump");
 
-	Zombie* Zombie2 = Zombie::newZombie("Zombie2");
+	Zombie* Zombie2 = newZombie("Zombie2");
 	if (Zombie2)
 	{
 		Zombie2->announce();
