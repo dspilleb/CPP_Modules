26327f60ea7fae848665933aa693b51cb56623da
dspilleb <spillebeendanilo@gmail.com>
2024-02-16 12:52:01 +0100

    small change

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dspilleb <dspilleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:48:52 by dspilleb          #+#    #+#             */
-/*   Updated: 2024/02/16 11:54:39 by dspilleb         ###   ########.fr       */
+/*   Updated: 2024/02/16 12:50:58 by dspilleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "Veuillez introduire un niveau de plainte de HARL :\n'DEBUG' | 'INFO' | 'WARNING' | 'ERROR'" << std::endl;
+		std::cout << "Veuillez introduire un niveau de plainte de HARL :\n'DEBUG' | 'INFO' | 'WARNING' | 'ERROR' ou une autre plainte" << std::endl;
 		return (1);
 	}
 	Harl my_harl;
