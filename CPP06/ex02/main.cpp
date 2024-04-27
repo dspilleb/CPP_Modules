210b5ed16577938d42cd4ea6bb9a3ac4de6f9fc9
Dan <spillebeendanilo@gmail.com>
2024-04-27 12:28:19 +0200

    Ex02

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,72 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+#include <ctime>
+
+Base * generate(void)
+{
+	int type = rand() % 3;
+
+	std::cout << "Generated: " << std::flush;
+	if (type == 0)
+	{
+		std::cout << "A" << std::endl;
+		return (new A());
+	}
+	else if (type == 1)
+	{
+		std::cout << "B" << std::endl;
+		return (new B());
+	}
+	else
+	{
+		std::cout << "C" << std::endl;
+		return (new C());
+	}
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p) != 0)
+		std::cout << "Type A !" << std::endl;
+	else if (dynamic_cast<B*>(p) != 0)
+		std::cout << "Type B !" << std::endl;
+	else
+		std::cout << "Type C !" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{ 
+		dynamic_cast<A&>(p);
+		std::cout << "Type A !" << std::endl;
+	}
+	catch (std::exception& E)
+	{
+		try
+		{
+			dynamic_cast<B&>(p);
+			std::cout << "Type B !" << std::endl;
+		}
+		catch(std::exception& E)
+		{
+			std::cout << "Type C !" << std::endl;
+		}
+	}
+}
+
+int main (void)
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+	Base *ptr = generate();
+
+	identify(ptr);
+	identify(*ptr);
+
+	delete ptr;
+
+	return (0);
+}
