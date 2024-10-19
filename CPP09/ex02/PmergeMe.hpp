9af00e867eed392f8fa4901a3ef48c770d681c5d
dspilleb <spillebeendanilo@gmail.com>
2024-10-19 17:57:52 +0200

    draft

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -7,23 +7,25 @@
 # include <cstdlib>
 # include <limits>
 # include <iterator>
-# include <algorithm> //TODO : remove after testing
+// # include <algorithm> //TODO : remove after testing
 # include <cmath>
 # include <utility>
+# include <vector>
+# include <iomanip>
 
 # define ERR_ARG "Error: Usage \"./PmergeMe <number_1> <number_2> ... <number_n>\" | <number_n> is a positive integer"
 # define ERR_NUM "Error: Argument is not a positive integer => "
 
 bool str_to_int(int &result, const char *str);
-bool parse_args(std::deque<int> &v, std::list<int> &l, const int &ac, char ** &av);
+bool parse_args(std::deque<int> &d, std::vector<int> &v, const int &ac, char ** &av);
 std::deque<int> Merge_Insertion_Sort_deque(std::deque<int> v);
+std::vector<int> Merge_Insertion_Sort_vector(std::vector<int> v);
 
 template <typename InputIterator>
 
 
 void print_container(InputIterator begin, InputIterator end)
 {
-	
 	while (begin != end)
 	{
 		std::cout << *begin << " " << std::flush;
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -4,22 +4,34 @@
 
 int main (int ac, char **av)
 {
-	std::deque<int> v;
-	std::list<int> l;
+	std::deque<int> d;
+	std::vector<int> v;
 
 	if (ac <= 1)
 	{
 		std::cerr << ERR_ARG << std::endl;
 		return (-1);
 	}
-	if (!parse_args(v, l, ac, av))
+	if (!parse_args(d, v, ac, av))
 		return (-1);
-	std::cout << "Before:  " << std::flush;
-	print_container(v.begin(), v.end());
-	v = Merge_Insertion_Sort_deque(v);
-	std::cout << "After:  " << std::flush;
-	print_container(v.begin(), v.end());
+	std::cout << std::fixed << std::setprecision(10);
 
+	std::cout << "Deque => Before:  " << std::flush;
+	print_container(d.begin(), d.end());
+
+	clock_t start = clock();
+	d = Merge_Insertion_Sort_deque(d);
+	clock_t end = clock();
+
+	std::cout << "After:  " << std::flush;
+
+	print_container(d.begin(), d.end());
+	std::cout << "Time to process a range of " << d.size() << " elements with std::deque : " << static_cast<double>(end - start) / (CLOCKS_PER_SEC * 1e6) <<" us" << std::endl;
+
+	start = clock();
+	v = Merge_Insertion_Sort_vector(v);
+	end = clock();
+	std::cout << "Time to process a range of " << d.size() << " elements with std::deque : " << static_cast<double>(end - start) / (CLOCKS_PER_SEC * 1e6) <<" us" << std::endl;
 
 	return (0);
 
